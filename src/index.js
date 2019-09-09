ba57faae9e5df4472d56ee0ce91a1f1b3f36ad2b
Jungir <googlman6300@gmail.com>
2019-09-09 13:52:13 +0600

    complex relational data w Arrays

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import { GraphQLServer } from 'graphql-yoga';
 
 //Demo user data
-const users = [
-    {id: 1, name: 'Kamil', email: 'kamil@thenetninja.co.uk', age: 23},
-    {id: 2, name: 'Kunau', email: 'kunau@thenetninja.co.uk', age: 23},
-    {id: 3, name: 'Jessy', email: 'jessy@thenetninja.co.uk', age: 30},
-    {id: 4, name: 'Clair', email: 'clair@thenetninja.co.uk'},
+const users = [  
+    {id: '2', name: 'Kunau', email: 'kunau@thenetninja.co.uk', age: 23},
+    {id: '3', name: 'Jessy', email: 'jessy@thenetninja.co.uk', age: 30},
+    {id: '4', name: 'Clair', email: 'clair@thenetninja.co.uk'},
+    {id: '1', name: 'Kamil', email: 'kamil@thenetninja.co.uk', age: 23},
 ]
 
 const posts = [
-    {id: 1, title: 'why cheese rules', body: 'the best post ever', published: false},
-    {id: 2, title: 'my boobs are large enough', body: 'can not stop reading', published: true},
-    {id: 3, title: 'look aside', body: 'where is the exit', published: true},
-    {id: 4, title: 'get out of my pc', body: 'stop being unoriginal', published: false}
+    {id: '1', title: 'why cheese rules', body: 'the best post ever', published: false, author: '1'},
+    {id: '2', title: 'my boobs are large enough', body: 'can not stop reading', published: true, author: '2'},
+    {id: '3', title: 'look aside', body: 'where is the exit', published: true, author: '3'},
+    {id: '4', title: 'get out of my pc', body: 'stop being unoriginal', published: false, author: '1'}
 ]
 // 5 main types:
 //String, Boolean, Int(whole numbers), Float(decimals), ID 
@@ -30,13 +30,14 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
     }
     type Post{
         id: ID!
         title: String!
         body: String!
         published: Boolean!
-        
+        author: User!
     }
 `
 //resolvers function, what to do when the query is asked
@@ -52,6 +53,8 @@ const resolvers = {
        },
        posts(parent, args, ctx, info){
             if(!args.query){
+                //graphQl expexts array of objects, each object is a parent with each new iteration
+                //if it can't resolve some of the fileds and then it goes into the other resolvers to see the match
                 return posts;
             }
             return posts.filter(value => {
@@ -73,8 +76,27 @@ const resolvers = {
                 title: 'the best graphQl ever',
                 body: 'year u should check this out. Lorem ipsum...',
                 published: false
-
             }
+        }
+    },
+    //for relational data query
+    //if it's not a scalar type we should teach grqph ql were to look for the resolver
+    Post:{
+        author(parent, args, ctx, info){
+            //find() return first instance
+            return users.find(user => {
+                //parent is the current object inside of a posts array that is dummy data
+                //parent is different each time; refers to the object in the array
+                return user.id === parent.author;
+            });
+        }
+    },
+    //for realtional data query
+    User:{
+        posts(parent, args, ctx, info){
+            return posts.filter(post => {
+                return parent.id === post.author;
+            });
         }
     }
 }
