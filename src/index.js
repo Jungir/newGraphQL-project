67140013eb5ea1064df5a5131233b8f8813d47b0
Jungir <googlman6300@gmail.com>
2019-09-09 11:48:34 +0600

    array 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+//Demo user data
+const users = [
+    {id: 1, name: 'Kamil', email: 'kamil@thenetninja.co.uk', age: 23},
+    {id: 2, name: 'Kunau', email: 'kunau@thenetninja.co.uk', age: 23},
+    {id: 3, name: 'Jessy', email: 'jessy@thenetninja.co.uk', age: 30},
+    {id: 4, name: 'Clair', email: 'clair@thenetninja.co.uk'},
+]
 
+const posts = [
+    {id: 1, title: 'why cheese rules', body: 'the best post ever', published: false},
+    {id: 2, title: 'my boobs are large enough', body: 'can not stop reading', published: true},
+    {id: 3, title: 'look aside', body: 'where is the exit', published: true},
+    {id: 4, title: 'get out of my pc', body: 'stop being unoriginal', published: false}
+]
 // 5 main types:
 //String, Boolean, Int(whole numbers), Float(decimals), ID 
 //type definitions (schema)
 // cannot have null with !
 const typeDefs = `
     type Query{
-        greeting(name:String): String!
         me: User!
         post: Post!
-        add(numbers: [Float!]!): Float!
-        grades: [Int!]!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
     }
     type User{
         id: ID!
@@ -30,17 +42,23 @@ const typeDefs = `
 //resolvers function, what to do when the query is asked
 const resolvers = {
     Query: {
-        grades(parent, args, ctx, info){
-            return [10, 20];
-        },
-        add(parent, args, ctx, info){
-            let numbers = args.numbers.length ? args.numbers : [];
-            return numbers.reduce((acc, curValue) => acc + curValue);
-        },
-        greeting(parent, args, ctx, info){
-            let name = args.name ? args.name : 'from the earth';
-            return 'Hello ' + name;
-        },
+       users(parent, args, ctx, info){
+            if(args.query){
+                return users.filter(curvalue => {
+                    return curvalue.name.toLowerCase().includes(args.query.toLowerCase());
+                });; 
+            }
+            return users;
+       },
+       posts(parent, args, ctx, info){
+            if(!args.query){
+                return posts;
+            }
+            return posts.filter(value => {
+                return value.title.toLowerCase().includes(args.query.toLowerCase()) || 
+                       value.body.toLowerCase().includes(args.query.toLowerCase()); 
+            });
+       },
         me(){
             return {
                 id: '1234', 
